7adf480199374c8480104d30460c6238b5eb845a
cactice <14835424+Cactice@users.noreply.github.com>
2021-02-25 09:31:21 +0900

    Fix for refactoring dir structure

--- bpy/install_sverchok.py
+++ bpy/install_sverchok.py
@@ -8,26 +8,24 @@ import bpy
 import requests
 
 
-def download_url(url: str, save_path: str, chunk_size=128):
-    """Download to a given path.
-
-    Args:
-        url (str): [description]
-        save_path (str): [description]
-        chunk_size (int, optional): [description]. Defaults to 128.
-    """
+def _download_url(url: str, save_path: str, chunk_size=128):
     req = requests.get(url, stream=True)
     with open(save_path, "wb") as fd:
         for chunk in req.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
 
 
-download_zip_file_path = "/usr/lib/master.zip"
-download_url(
-    "https://github.com/nortikin/sverchok/archive/master.zip",
-    download_zip_file_path,
-)
+def _main():
+    download_zip_file_path = "/usr/lib/master.zip"
+    _download_url(
+        "https://github.com/nortikin/sverchok/archive/master.zip",
+        download_zip_file_path,
+    )
 
-bpy.ops.preferences.addon_install(filepath=download_zip_file_path)
-bpy.ops.preferences.addon_enable(module="sverchok-master")
-bpy.ops.wm.save_userpref()
+    bpy.ops.preferences.addon_install(filepath=download_zip_file_path)
+    bpy.ops.preferences.addon_enable(module="sverchok-master")
+    bpy.ops.wm.save_userpref()
+
+
+if __name__ == "__main__":
+    _main()
