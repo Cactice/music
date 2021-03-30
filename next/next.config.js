b0ae3590e277d4a609e20bee6849b3f06daf89ff
cactice <14835424+Cactice@users.noreply.github.com>
2021-03-30 13:43:07 +0000

    Fix config files

--- next/next.config.js
+++ next/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+    return config
+  },
   "parser": "@typescript-eslint/parser",
   "extends": [
     "plugin:@typescript-eslint/recommended",
