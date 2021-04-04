8831c7197cf9d4ea2070a37111f59a080b9bdbdc
cactice <14835424+Cactice@users.noreply.github.com>
2021-04-04 21:59:03 +0900

    Update env var for next

--- next/next.config.js
+++ next/next.config.js
@@ -1,26 +1,25 @@
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.node = {
-        fs: 'empty'
-      }
+        fs: 'empty',
+      };
     }
-    return config
+    return config;
   },
-  basePath: "/sverchok-models",
-  "parser": "@typescript-eslint/parser",
-  "extends": [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended"
+  basePath: isProd ? '/sverchok-models' : '/',
+  assetPrefix: isProd ? '/sverchok-models/' : '/',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
   ],
-  "plugins": [
-    "@typescript-eslint",
-    "react"
-  ],
-  "rules": {
-    "react/react-in-jsx-scope": "off"
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
   },
-  "globals": {
-    "React": "writable"
-  }
-}
+  globals: {
+    React: 'writable',
+  },
+};
